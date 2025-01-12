56c2715dfb2b44568df372426db348e841b44bd1
Wilgrey Ravelo Cruz <167430214+Wilgrey2004@users.noreply.github.com>
2025-01-11 22:01:06 -0400

    Updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        Mover: "Mover 4s infinite linear",
+      },
+      keyframes: {
+        Mover: {
+          "0%": {
+            transform: "translate(0px, 0px) scale(1)",
+          },
+          "33%": {
+            transform: "translate(10px, 10px) scale(1.2)",
+          },
+          "66%": {
+            transform: "translate(15px, 15px) scale(0.8)",
+          },
+          "100%": {
+            transform: "translate(0px, 0px) scale(1)",
+          },
+        },
+      },
+    },
   },
   plugins: [],
 };
