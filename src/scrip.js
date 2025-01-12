361a46edebf3a22c4542bfbfe3a4e08e9a310686
Wilgrey Ravelo Cruz <167430214+Wilgrey2004@users.noreply.github.com>
2025-01-11 22:13:04 -0400

    Esta es la version para mi amada jajaaja

---
+++ src/scrip.js
@@ -0,0 +1,10 @@
+const btnUno = document.getElementById("btnUno");
+const btnDos = document.getElementById("btnDos");
+
+btnUno.addEventListener("click", () => {
+  alert("te amo, te quiero mucho y te quiero mucho guapaaaaaaa!!!!!");
+});
+
+btnDos.addEventListener("click", () => {
+  alert("gracias por clicarme, ahora soy tuyo, te amamos hermosaaaaaa!");
+});
